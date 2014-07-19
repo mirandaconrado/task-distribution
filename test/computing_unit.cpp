841b5d0109fe33e64a90423939749734665da537
Conrado Miranda <miranda.conrado@gmail.com>
2014-07-19 20:41:27 -0300

    Added test for default flags in ComputingUnit.

--- test/computing_unit.cpp
+++ test/computing_unit.cpp
@@ -2,7 +2,25 @@
 
 #include <gtest/gtest.h>
 
-TEST(ComputingUnit, IdentityComputingUnit) {
+class TestComputingUnit: public TaskDistribution::ComputingUnit<TestComputingUnit> {
+  public:
+    typedef int result_type;
+    typedef std::tuple<int> args_type;
+    static const std::string name;
+
+    result_type operator()(args_type const& args) const {
+      return 2*std::get<0>(args);
+    }
+};
+const std::string TestComputingUnit::name("TestComputingUnit");
+
+TEST(ComputingUnit, Defaults) {
+  TestComputingUnit unit;
+  EXPECT_FALSE(unit.run_locally());
+  EXPECT_TRUE(unit.should_save());
+}
+
+TEST(ComputingUnit, Identity) {
   TaskDistribution::IdentityComputingUnit<int> unit;
   EXPECT_TRUE(unit.run_locally());
   EXPECT_FALSE(unit.should_save());
