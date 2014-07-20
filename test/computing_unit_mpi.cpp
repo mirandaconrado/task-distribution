a99c62c98ab105290e57662a053767c29999d95d
Conrado Miranda <miranda.conrado@gmail.com>
2014-07-20 15:07:23 -0300

    Updated ComputingUnit MPI test to handle execution.

--- test/computing_unit_mpi.cpp
+++ test/computing_unit_mpi.cpp
@@ -3,5 +3,23 @@
 
 #include <gtest/gtest.h>
 
-TEST(MPIComputingUnit, Dummy) {
+TEST(MPIComputingUnit, Execute) {
+  TestComputingUnit unit;
+  if (world.rank() == 0) {
+    size_t task_id = 123456;
+    world.send(1, 0, unit);
+    world.send(1, 0, task_id);
+    world.send(1, 0, std::make_tuple(1));
+
+    int received_return;
+    size_t received_task_id;
+    world.recv(1, 0, received_task_id);
+    world.recv(1, 0, received_return);
+
+    EXPECT_EQ(task_id, received_task_id);
+    EXPECT_EQ(received_return, 2);
+  }
+  else {
+    unit.execute(world);
+  }
 }
