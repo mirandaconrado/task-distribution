95f04c812a2587ed17b2524e7524d92c372128aa
Conrado Miranda <miranda.conrado@gmail.com>
2014-08-05 10:01:38 -0300

    [reorganization] Updated test.

--- test/task_manager.cpp
+++ test/task_manager.cpp
@@ -5,6 +5,7 @@
 TEST(TaskManager, IdentityTask) {
   TaskDistribution::TaskManager task_manager;
   auto task = task_manager.new_identity_task(1);
-  auto task3 = task_manager.new_identity_task(1);
+  TaskDistribution::Task<float> task3 = task_manager.new_identity_task(1);
   auto task2 = task_manager.new_identity_task(task);
+  TaskDistribution::Task<float> task4 = task2;
 }
