2ff4f4e5c21a175e669fe3eb4584a886a4cb1c46
Conrado Miranda <miranda.conrado@gmail.com>
2014-08-26 10:39:42 -0300

    Task clean-up.

--- include/task.hpp
+++ include/task.hpp
@@ -1,11 +1,6 @@
 #ifndef __TASK_DISTRIBUTION__TASK_HPP__
 #define __TASK_DISTRIBUTION__TASK_HPP__
 
-//#include <boost/any.hpp>
-/*#if !(NO_MPI)
-#include <boost/mpi/communicator.hpp>
-#endif*/
-
 #include "key.hpp"
 
 namespace TaskDistribution {
@@ -21,27 +16,11 @@ namespace TaskDistribution {
         task_manager_ = other.task_manager_;
       }
 
-      /*template <class Other>
-      Task(Task<Other> const& other) {
-        *this = task_manager_->template new_conversion_task<T>(other);
-      }*/
-
       Task<T> const& operator=(Task<T> const& other) {
         task_key_ = other.task_key_;
         task_manager_ = other.task_manager_;
         return *this;
       }
-
-      /*template <class Other>
-      Task<T> const& operator=(Task<Other> const& other) {
-        *this = task_manager_->template new_conversion_task<T>(other);
-        return *this;
-      }*/
-
-      /*template<class Archive>
-      void serialize(Archive& ar, const unsigned int version) {
-        ar & task_key_;
-      }*/
 
       // Can't use operator== as it may force type conversion
       bool is_same_task(Task<T> const& other) const {
@@ -61,11 +40,7 @@ namespace TaskDistribution {
       }
 
     private:
-      //template <class> friend class Task;
-
       friend class TaskManager;
-
-      //friend struct DependencyAnalyzer;
 
       Task(Key task_key, TaskManager* task_manager):
         task_key_(task_key),
